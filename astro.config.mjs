a14428dca053da892efc5ebe95fbc0892efb7abe
luisbbq <luis@bbqing.com>
2023-12-28 13:36:03 -0500

    Added moving logo and better colors

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
+import lottie from "astro-integration-lottie";
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+	integrations: [lottie()],
 	site: "https://ottagua.com",
 });
