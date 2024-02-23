d16fa1b9f056436ecdb0c0c71b39bac7321f0f24
luisbbq <luis@bbqing.com>
2024-02-23 12:35:42 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://ottagua.com",
-	output: "local",
+	output: "hybrid",
 	integrations: [lottie(), mdx(), sitemap()],
 });
