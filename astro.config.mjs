520dd715d9cca697e4e0e2d420c458ca304366d8
luisbbq <luis@bbqing.com>
2024-02-23 12:16:44 -0500

    ERmoved Kaystatic

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,9 @@ import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
 
-import keystatic from "@keystatic/astro";
-
 // https://astro.build/config
 export default defineConfig({
 	site: "https://ottagua.com",
-	output: "local",
-	integrations: [lottie(), mdx(), sitemap(), react(), markdoc(), keystatic()],
+	output: "hybrid",
+	integrations: [lottie(), mdx(), sitemap(), react(), markdoc()],
 });
