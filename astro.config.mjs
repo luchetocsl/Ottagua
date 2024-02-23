93ebb6c516a5b7d741717d4a4367dd547f4548a8
luisbbq <luis@bbqing.com>
2024-02-23 12:23:31 -0500

    Revert "ERmoved Kaystatic"
    This reverts commit 520dd715d9cca697e4e0e2d420c458ca304366d8.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,11 @@ import sitemap from "@astrojs/sitemap";
 import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
 
+import keystatic from "@keystatic/astro";
+
 // https://astro.build/config
 export default defineConfig({
 	site: "https://ottagua.com",
-	output: "hybrid",
-	integrations: [lottie(), mdx(), sitemap(), react(), markdoc()],
+	output: "local",
+	integrations: [lottie(), mdx(), sitemap(), react(), markdoc(), keystatic()],
 });
