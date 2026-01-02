13c068d5975ec94a54d6dc88730fa8f28c76b3c0
Luis Martinez <67282267+luchetocsl@users.noreply.github.com>
2026-01-01 20:54:42 -0500

    changed direction of the site

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,18 +1,20 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
-	type: 'content',
+	type: "content",
 	// Type-check frontmatter using a schema
-	schema: ({ image }) => z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: image().optional(),
-		slug: z.string().optional(),
-		tags: z.array(z.string()).optional(),
-	}),
+	schema: ({ image }) =>
+		z.object({
+			title: z.string(),
+			description: z.string(),
+			summary: z.string().optional(),
+			// Transform string to Date object
+			pubDate: z.coerce.date(),
+			updatedDate: z.coerce.date().optional(),
+			heroImage: image().optional(),
+			slug: z.string().optional(),
+			tags: z.array(z.string()).optional(),
+		}),
 });
 
 export const collections = { blog };
