d6c394bd4eefddad1648a14ea83543e2766fb107
Luis Martinez <67282267+luchetocsl@users.noreply.github.com>
2026-04-14 05:13:58 -0400

    updated astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,19 +3,10 @@ import cloudflare from "@astrojs/cloudflare";
 import mdx from "@astrojs/mdx";
 import lottie from "astro-integration-lottie";
 import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://ottagua.com",
 	adapter: cloudflare(),
-	integrations: [
-		lottie(),
-		mdx(),
-		sitemap(),
-		tailwind({
-			applyBaseStyles: false,
-			configFile: "./tailwind.config.js",
-		}),
-	],
+	integrations: [lottie(), mdx(), sitemap()],
 });
